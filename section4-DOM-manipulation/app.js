404d4797d776d1a9c57cce06cd5873b5f7ae3af3
Marcia Tobias <marciaatobiass@gmail.com>
2019-07-03 21:52:29 +0100

    Update app.js file
    Created action for roll bottom and scores setted to 0.

--- section4-DOM-manipulation/app.js
+++ section4-DOM-manipulation/app.js
@@ -9,26 +9,58 @@
 */
 
 // Array for the scores.
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 // initialize variables.
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-// generates a random number from 0 to 6 and math.floor for integer number.
-dice = Math.floor(Math.random() * 6) + 1;
+
 
 // select the id, selected the element, text content receive the variable dice.
 // query selector, to change, to maipulate values and elements of the page.
 // setter
-document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).textContent = dice;
 // if we want write the inner html, we have to use string.
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
 
 // getter
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
+//var x = document.querySelector('#score-0').textContent;
+//console.log(x);
 
 // Style method, display property in css
 document.querySelector('.dice').style.display = 'none';
+
+// we can also select elements using get element by id.
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+//function btn() {
+//    
+//}
+//
+//btn();
+
+// query selector will select the roll bottom
+// Add eventListener, will add an event after the click of the mouse. 
+// call back, that call the function.
+// anonymous function is without name and cannot be reused.
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    
+    // generates a random number from 0 to 6 and math.floor for integer number.
+    // just this function will have access to the dice variable.
+    // 1. Random number
+    dice = Math.floor(Math.random() * 6) + 1;
+    
+    // 2. Display the result.
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png'
+    
+    
+    // 3. Update the round score if the rolled number was NOT 1.
+});
+
