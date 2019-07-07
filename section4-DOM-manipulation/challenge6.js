af3cbb51c914270a33fcab1e73dd69961a9f236f
Marcia Tobias <marciaatobiass@gmail.com>
2019-07-07 20:49:25 +0100

    Update challenge6 file
    Adding the second item for the challenge.

--- section4-DOM-manipulation/challenge6.js
+++ section4-DOM-manipulation/challenge6.js
@@ -8,6 +8,16 @@
 
 */
 
+/*
+YOUR 3 CHALLENGES
+Change the game to follow these rules:
+1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
+
+2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good opportunity to use google to figure this out :)
+
+3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
+*/
+
 // Array for the scores.
 var scores, roundScore, activePlayer, gamePlaying;
 
@@ -87,9 +97,22 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     
         // update the UI
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+        
+        var input = document.querySelector('.final-score').value;
+        var winningScore;
+        
+        // undefined, ), null or "" are COERCED to false
+        // anything else is COERCED to true
+        if(input) {
+            
+            var winningScore = input;     
+            
+        } else {
+            winningScore = 100;
+        }
     
         // Check if player won the game
-        if(scores[activePlayer] >= 100) {
+        if(scores[activePlayer] >= winningScore) {
         
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none';
@@ -100,8 +123,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     
         } else {
             nextPlayer();
-        }
-        
+        } 
     }
 });
 
