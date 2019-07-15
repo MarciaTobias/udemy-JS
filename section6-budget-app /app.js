b5f45e0d5e4ee530b5e74e1b5ad342fbb12eba91
Marcia Tobias <marciaatobiass@gmail.com>
2019-07-15 21:40:25 +0100

    Create Budget files
    Adding files for new program.

---
+++ section6-budget-app /app.js
@@ -0,0 +1,16 @@
+var budgetController = (function {
+                        
+    var x = 23;
+    var add = function(a) {
+    
+    return x + a;
+}
+
+return {
+    
+    publicTest: function(b) {
+        console.log(add(b));
+    }
+}
+                        
+}) ();
