b5dd9b7142856dce32a42697eae6a03d525af6f6
Marcia Tobias <marciaatobiass@gmail.com>
2019-09-18 17:47:18 +0100

    Data model implemented

--- section9-forkify/src/js/index.js
+++ section9-forkify/src/js/index.js
@@ -1,18 +1,4 @@
-// conversion give the same like the code
-import axios from 'axios';
+import Search from './modules/Search';
 
-async function getResults(query) {
-    // that one is better than fetch, return automaticaly json
-    const key = 'fc9c16f3c0fdccce9fd63e9003aeb034';
-
-    try {
-        // q cames from the api
-        // that is the way to handle the api
-        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    } catch (error) {
-        alert(error)
-    }
-}
-getResults('tomato pasta');
+const search = new Search('pizza');
+console.log(search);
--- section9-forkify/src/js/modules/Search.js
+++ section9-forkify/src/js/modules/Search.js
@@ -1,2 +1,25 @@
+// conversion give the same like the code
+import axios from 'axios';
+
 // export dedault we simple put an expression
-export default 'I am an export string.';
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        // that one is better than fetch, return automaticaly json
+        const key = 'fc9c16f3c0fdccce9fd63e9003aeb034';
+    
+        try {
+            // q cames from the api
+            // that is the way to handle the api
+            const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            console.log(res);
+            this.result = res.data.recipes;
+            console.log(this.result);
+        } catch (error) {
+            alert(error)
+        }
+    }
+}
