ab8b0b4425393e4ee2e4670c730e795c6b36dff5
Marcia Tobias <marciaatobiass@gmail.com>
2019-09-18 17:36:05 +0100

    Correct api and included try-cacth

--- section9-forkify/src/js/index.js
+++ section9-forkify/src/js/index.js
@@ -4,10 +4,15 @@ import axios from 'axios';
 async function getResults(query) {
     // that one is better than fetch, return automaticaly json
     const key = 'fc9c16f3c0fdccce9fd63e9003aeb034';
-    // q cames from the api
-    // that is the way to handle the api
-    const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-    const recipes = res.data.recipes;
-    console.log(recipes);
+
+    try {
+        // q cames from the api
+        // that is the way to handle the api
+        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch (error) {
+        alert(error)
+    }
 }
-getResults();
+getResults('tomato pasta');
