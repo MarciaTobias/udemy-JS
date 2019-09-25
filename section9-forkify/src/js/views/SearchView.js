b8acb7dfe4a1694dddcaceda34a5a68479095a9f
Marcia Tobias <marciaatobiass@gmail.com>
2019-09-25 17:46:49 +0100

    Implemented limit for the title of recipes

--- section9-forkify/src/js/views/SearchView.js
+++ section9-forkify/src/js/views/SearchView.js
@@ -1,4 +1,5 @@
 import { elements } from './base';
+import { pathToFileURL } from 'url';
 
 // The return is implicity, when is just one line of code in the arrow function
 export const getInput = () => elements.searchInput.value;
@@ -13,6 +14,32 @@ export const clearInput = () => {
     elements.searchInput.value = '';
 };
 
+/*
+// In the first iteration, the accumaltor is zero
+For examplo, 'Pasta with tomato and spinach'
+acc: 0 / acc + cur.length = 5 / newTitle = ['Pasta]
+acc: 5 / acc + cur.length = 9 / newTitle = ['Pasta', 'with']
+acc: 9 / acc + cur.length = 15 / newTitle = ['Pasta', 'with', 'tomato']
+acc: 15 / acc + cur.length = 18 / newTitle = ['Pasta', 'with', 'tomato']
+*/
+
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if (title.length > limit) {
+        title.split(' ').reduce((acc, cur) => {
+
+            if (acc + cur.length <= limit) {
+                newTitle.push(cur);
+            }
+            return acc + cur.length;                
+        }, 0);
+
+        // return the result
+        return `${newTitle.join(' ')} ...`;
+    }
+    return title;
+}; 
+
 // this function will recive um recipe, private function
 const renderRecipe  = recipe => {
     const markupp = `
@@ -22,7 +49,7 @@ const renderRecipe  = recipe => {
                     <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">${recipe.title}</h4>
+                    <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                     <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
