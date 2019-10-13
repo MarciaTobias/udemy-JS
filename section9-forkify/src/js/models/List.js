c8e8dd353e2319925e7264ddbea66bba24b85bc6
Marcia Tobias <marciaatobiass@gmail.com>
2019-10-13 21:23:16 +0100

    Create list.js

---
+++ section9-forkify/src/js/models/List.js
@@ -0,0 +1,11 @@
+export default class List {
+    constructor() {
+        // All of the elements in our list will all be stored in this one property, this.items.
+        // Each elemenet in the array is an object which will have the count, the unit, and the ingredient.
+        this.items = [];
+    }
+
+    addItem (count, unit, ingredient) {
+
+    }
+}
