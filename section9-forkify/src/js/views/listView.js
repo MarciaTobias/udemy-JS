1c1ff91939454a6566e01e8c1394fedd43e23c00
Marcia Tobias <marciaatobiass@gmail.com>
2019-10-22 10:11:29 +0100

    Update listView

--- section9-forkify/src/js/views/listView.js
+++ section9-forkify/src/js/views/listView.js
@@ -5,9 +5,9 @@ export const renderItem = item => {
         <li class="shopping__item" data-itemid=${item.id}>
             <div class="shopping__count">
                 <input type="number" value="${item.count}" step="${item.count}" class="shopping__count-value">
-                <p>"${item.unit}"</p>
+                <p>${item.unit}</p>
             </div>
-            <p class="shopping__description">"${item.ingredient}</p>
+            <p class="shopping__description">${item.ingredient}</p>
             <button class="shopping__delete btn-tiny">
                 <svg>
                     <use href="img/icons.svg#icon-circle-with-cross"></use>
