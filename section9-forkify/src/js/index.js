53ac2939b3c543becf9ea8088f4771b66bd61435
Marcia Tobias <marciaatobiass@gmail.com>
2019-11-03 11:47:31 +0000

    Update index

--- section9-forkify/src/js/index.js
+++ section9-forkify/src/js/index.js
@@ -191,10 +191,6 @@ elements.shopping.addEventListener('click', e => {
  * LIKE CONTROLLER
  */
 
- // Testing
-state.likes = new Likes();
-likesView.toggleLikeMenu(state.likes.getNumLikes());
-
 const controlLike = () => {
     if (!state.likes) state.likes = new Likes();
     const currentID = state.recipe.id;
@@ -230,6 +226,20 @@ const controlLike = () => {
     }
     likesView.toggleLikeMenu(state.likes.getNumLikes());
 };
+
+// Restore liked recipes on page load
+window.addEventListener('load', () => {
+    state.likes = new Likes();
+
+    // Restore likes
+    state.likes.readStorage();
+
+    // Toggle like menu button
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+    // Render the existent likes
+    state.likes.likes.forEach(like => likesView.renderLike(like));
+});
 
 
 // Handling recipe button clicks.
